7feb99432c2e8dbf5fd8311c54d4fbfa7a36713d
ashraf74 <ahmedashraaf09@gmail.com>
2024-10-01 00:52:46 +0300

    Configure Translation using reverso

--- background.js
+++ background.js
@@ -1,20 +1,31 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'translate') {
-        fetch('https://translatly.glitch.me/translate', {
+        fetch('https://api.reverso.net/translate/v1/translation', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'accept': 'application/json, text/plain, */*',
+                'content-type': 'application/json',
+                'origin': 'https://www.reverso.net',
+                'referer': 'https://www.reverso.net/',
+                'x-reverso-origin': 'translation.web',
             },
             body: JSON.stringify({
-                q: message.text,
-                targetLanguage: message.targetLanguage,
+                format: "text",
+                from: message.sourceLanguage || "eng",  // Default to English if not provided
+                to: message.targetLanguage || "ara",    // Default to Arabic if not provided
+                input: message.text,
+                options: {
+                    sentenceSplitter: true,
+                    origin: "translation.web",
+                    contextResults: true,
+                    languageDetection: true
+                }
             }),
         })
         .then(response => response.json())
         .then(data => {
-            console.log("Translation API Response:", data);  // Log the API response
-            if (data.translatedText) {
-                sendResponse({ translatedText: data.translatedText });
+            if (data.translation) {
+                sendResponse({ translatedText: data.translation[0] });
             } else {
                 console.error('No translated text received');
                 sendResponse({ translatedText: null });
